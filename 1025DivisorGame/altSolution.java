5ff03cba788e87cb10af628a284e5a26a8a7b3c5
Andrew Chen <andrew47ohio@gmail.com>
2021-01-12 21:16:44 -0500

    Divisor Game

---
+++ 1025DivisorGame/altSolution.java
@@ -0,0 +1,9 @@
+class altSolution {
+    public boolean divisorGame(int N) {
+        boolean flag = false;
+        if (N%2 == 0){
+            flag = true;
+        }
+        return flag;
+    }
+}
