c1a79758446d39a9da68e82495d01b6d152d587f
Andrew Chen <andrew47ohio@gmail.com>
2021-01-17 10:38:26 -0500

    Target Sum

---
+++ 494TargetSum/Solution.java
@@ -0,0 +1,26 @@
+class Solution{
+    public int findTargetSumWays(int[] nums, int S){
+        int summation;
+        int[] dp = new int[2001];
+        dp[nums[0] + 1000] = 1;
+        dp[-nums[0] + 1000] += 1;
+        for (int i = 1; i < nums.length; i++){
+            int[] next = new int[2001];
+            for (int sum = -1000; sum <= 1000; sum++){
+                if (dp[sum+1000] > 0){
+                    next[sum + nums[i] + 1000] += dp[sum+ 1000];
+                    next[sum - nums[i] + 1000] += dp[sum+ 1000];
+                }
+            }
+            dp = next;
+        }
+        if (S > 1000){
+            summation = 0;
+        }
+        else {
+            summation = dp[S+ 1000];
+        }
+        return summation;
+
+    }
+}
