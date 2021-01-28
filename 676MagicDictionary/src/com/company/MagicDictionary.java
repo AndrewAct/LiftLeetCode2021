2f6880df80855f0f31ef2286f443acc2daf66a09
Andrew Chen <andrew47ohio@gmail.com>
2021-01-27 21:05:54 -0500

    Magic Dictionary

---
+++ 676MagicDictionary/src/com/company/MagicDictionary.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class MagicDictionary {
+    Map<String, List<int[]>> map = new HashMap<>();
+    /** Initialize your data structure here. */
+    public MagicDictionary() {
+
+    }
+
+    public void buildDict(String[] dictionary) {
+        for (String s: dictionary){
+            for (int i = 0; i < s.length(); i++){
+                String key = s.substring(0,i)+ s.substring(i+1);
+                int[] pair = new int[] {i, s.charAt(i)};
+                List<int[]> val = map.getOrDefault(key, new ArrayList<int[]>());
+                val.add(pair);
+                map.put(key, val);
+            }
+        }
+
+    }
+
+    public boolean search(String searchWord) {
+        for (int i = 0; i < searchWord.length(); i++){
+            String key = searchWord.substring(0, i) + searchWord.substring(i+1);
+            if (map.containsKey(key)){
+                for (int[] pair: map.get(key)){
+                    if (pair[0] == i && pair[1] != searchWord.charAt(i)) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.buildDict(dictionary);
+ * boolean param_2 = obj.search(searchWord);
+ */
