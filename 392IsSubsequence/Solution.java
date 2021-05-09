1713950a5eeac524d75b9109801cb24557cdb801
Andrew Chen <andrew47ohio@gmail.com>
2021-05-09 19:21:24 -0400

    Is Suubsequence

--- 392IsSubsequence/Solution.java
+++ 392IsSubsequence/Solution.java
@@ -1,14 +1,29 @@
+//class Solution {
+//    public boolean isSubsequence(String s, String t) {
+//        Integer leftBound = s.length(), rightBound = t.length();
+//        Integer left = 0, right = 0;
+//        while (left < leftBound && right < rightBound){
+//            if (s.charAt(left) == t.charAt(right)){
+//                left += 1;
+//            }
+//            right += 1;
+//        }
+//        return left == leftBound;
+//
+//    }
+//}
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Integer leftBound = s.length(), rightBound = t.length();
-        Integer left = 0, right = 0;
-        while (left < leftBound && right < rightBound){
-            if (s.charAt(left) == t.charAt(right)){
-                left += 1;
-            }
-            right += 1;
+        int leftBound = s.length(), rightBound = t.length();
+        int i = 0, j = 0;
+        while (i < leftBound && j < rightBound) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else j++;
         }
-        return left == leftBound;
+        if (i == leftBound) return true;
+        else return false;
 
     }
 }
